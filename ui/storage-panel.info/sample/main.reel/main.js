e35e2b92983858d9f47599aa5de4cfddf6888f54
gegao0124 <ge@montagestudio.com>
2015-02-19 12:57:14 -0800

    changed bindings to metadata description

--- ui/storage-panel.info/sample/main.reel/main.js
+++ ui/storage-panel.info/sample/main.reel/main.js
@@ -18,27 +18,27 @@ exports.Main = Component.specialize(/** @lends Main# */ {
             this.super();
             this.cache = new Cache();
             this.cache.entry = new CacheEntry();
-            this.cache.entry.data = 'Local Data';
-            this.cache.entry.metadata = {"type": "", "dataSize": 100};
+            this.cache.entry.data = {};
+            this.cache.entry.metadata = {"type": "", "dataSize": 100, "description": "Local Data"};
             this.cache.entry.subentries = [new CacheEntry(), new CacheEntry()];
-            this.cache.entry.subentries[0].data = "Region1";
-            this.cache.entry.subentries[0].metadata = {"type":"Media", "dataSize": 40};
+            this.cache.entry.subentries[0].data = {};
+            this.cache.entry.subentries[0].metadata = {"type":"Media", "dataSize": 40, "description": "Region1"};
             this.cache.entry.subentries[0].subentries = [new CacheEntry(), new CacheEntry(), new CacheEntry()];
-            this.cache.entry.subentries[0].subentries[0].data = "Pictures";
-            this.cache.entry.subentries[0].subentries[0].metadata = {"type":"jpg", "dataSize": 15};
-            this.cache.entry.subentries[0].subentries[1].data = "Videos";
-            this.cache.entry.subentries[0].subentries[1].metadata = {"type":"mp4", "dataSize": 20};
-            this.cache.entry.subentries[0].subentries[2].data = "Information";
-            this.cache.entry.subentries[0].subentries[2].metadata = {"type":"txt", "dataSize": 5};
-            this.cache.entry.subentries[1].data = "Region2";
-            this.cache.entry.subentries[1].metadata = {"type":"Media", "dataSize": 60};
+            this.cache.entry.subentries[0].subentries[0].data = {};
+            this.cache.entry.subentries[0].subentries[0].metadata = {"type":"jpg", "dataSize": 15, "description": "Pictures"};
+            this.cache.entry.subentries[0].subentries[1].data = {};
+            this.cache.entry.subentries[0].subentries[1].metadata = {"type":"mp4", "dataSize": 20, "description": "Videos"};
+            this.cache.entry.subentries[0].subentries[2].data = {};
+            this.cache.entry.subentries[0].subentries[2].metadata = {"type":"txt", "dataSize": 5, "description": "Information"};
+            this.cache.entry.subentries[1].data = {};
+            this.cache.entry.subentries[1].metadata = {"type":"Media", "dataSize": 60, "description": "Region2"};
             this.cache.entry.subentries[1].subentries = [new CacheEntry(), new CacheEntry(), new CacheEntry()];
-            this.cache.entry.subentries[1].subentries[0].data = "Pictures";
-            this.cache.entry.subentries[1].subentries[0].metadata = {"type":"jpg", "dataSize": 18};
-            this.cache.entry.subentries[1].subentries[1].data = "Videos";
-            this.cache.entry.subentries[1].subentries[1].metadata = {"type":"mp4", "dataSize": 40};
+            this.cache.entry.subentries[1].subentries[0].data = {};
+            this.cache.entry.subentries[1].subentries[0].metadata = {"type":"jpg", "dataSize": 18, "description": "Pictures"};
+            this.cache.entry.subentries[1].subentries[1].data = {};
+            this.cache.entry.subentries[1].subentries[1].metadata = {"type":"mp4", "dataSize": 40, "description": "Videos"};
             this.cache.entry.subentries[1].subentries[2].data = "Information";
-            this.cache.entry.subentries[1].subentries[2].metadata = {"type":"txt", "dataSize": 2};
+            this.cache.entry.subentries[1].subentries[2].metadata = {"type":"txt", "dataSize": 2, "description": "Information"};
         }
     },
     cache: {
