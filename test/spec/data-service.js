0a61d569a52c4818729823ea7e7aebb7ab9d0455
Charles d'Harcourt <dharcourt@users.noreply.github.com>
2015-08-14 19:19:41 -0700

    Simplify to publish initial NPM package
    All incomplete code was removed, leaving only README and LICENSE files.
    The incomplete code will be restored with a revert of this commit after
    the package is published.

--- test/spec/data-service.js
+++
@@ -1,7 +0,0 @@
-var DataService = require("montage-data/logic/service/data-service").DataService;
-
-describe("A DataService", function() {
-
-    xit ("needs to have tests!", function () {});
-
-});
--- test/spec/object-descriptor.js
+++
@@ -1,9 +0,0 @@
-var ObjectDescriptor = require("montage-data/logic/model/object-descriptor").ObjectDescriptor;
-
-describe("An ObjectDescriptor", function() {
-
-    it ("can be created", function () {
-        expect(new ObjectDescriptor()).toBeDefined();
-    });
-
-});
